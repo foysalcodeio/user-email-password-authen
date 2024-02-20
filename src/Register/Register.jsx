1d79e0196d0ef2c31c45d73248fff2333d308cee
Md.Foysal Ahammed <md.foysalahammedforhad@gmail.com>
2024-02-20 20:48:54 +0600

    error check

--- src/Register/Register.jsx
+++ src/Register/Register.jsx
@@ -3,7 +3,8 @@ import auth from "../firebase/firebase.config";
 import { useState } from "react";
 
 const Register = () => {
-    const [registerError, setRegisterError] = useState(' ')
+    const [registerError, setRegisterError] = useState(' ');
+    const [success, setSuccess] = useState('');
 
     const handleRegister = e => {
         e.preventDefault();
@@ -11,14 +12,25 @@ const Register = () => {
         const password = e.target.password.value;
         // console.log(email, password);
 
-
         //reset error
-        setRegisterError(' ');
+        setRegisterError('');
+        //success message reset
+        setSuccess('');
+
+
+        if(password.length < 6){
+            setRegisterError('password should be at least 6 character or longer');
+            return; // ae function por sa ar agabe na / caracter kom hole pore ar agabe na / mane konsole e error print ar hobe na
+            // client side e validation kore falsi
+            
+        }
 
         
+
         createUserWithEmailAndPassword(auth, email, password)
         .then( (result) => {
             console.log(result.user);
+            setSuccess('user created successfully');
         } )
         .catch( (error) => {
         
@@ -31,7 +43,7 @@ const Register = () => {
             <div className="mx-auto md:w-1/2">
             <h2 className="text-3xl text-black mt-5 mb-5">Please Register</h2>
             <form onSubmit={handleRegister} >
-                <input className="mb-4 text-white p-4 w-3/4" type="email" name="email" placeholder="foysal@yahoo.com" id="" />
+                <input className="mb-4 text-white p-4 w-3/4" type="email" name="email" placeholder="foysal@yahoo.com" id="" required />
                 <br />
                 <input className="text-white p-4 w-3/4" type="password" name="password" placeholder="foysal#123" id="" />
                 <br />
@@ -39,6 +51,9 @@ const Register = () => {
             </form>
             {
                 registerError && <p className="text-red-500">{registerError}</p>
+            }
+            {
+                success && <p className="text-green-700">{success}</p>
             }
             </div>
         </div>
